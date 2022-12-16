c0a58635d32452df66dfcb6378435dffcd011de7
max-muoto <41130755+max-muoto@users.noreply.github.com>
2022-12-15 19:33:53 -0600

    Move dijkstras over to new graph class

--- dijkstras.py
+++ dijkstras.py
@@ -1,88 +1,46 @@
-from typing import Dict
 from queue import PriorityQueue
 
-
-class Node:
-    weights: Dict[str, int] = {}
-
-    def __init__(self, name):
-        self.name = name
-        self.weights = {}
-
-    def add_weight(self, node, weight):
-        self.weights[node] = weight
-
-    def get_weight(self, node):
-        return self.weights[node]
-
-    def get_weights(self):
-        return self.weights
+from graph import Graph, GraphType
 
 
-class Graph:
-    nodes: Dict[str, Node] = {}
-
-    def add_edge(self, node1, node2, weight):
-        if node1 in self.nodes:
-            self.nodes[node1].add_weight(node2, weight)
-        else:
-            first_node = Node(node1)
-            first_node.add_weight(node2, weight)
-            self.nodes[node1] = first_node
-
-        if node2 in self.nodes:
-            self.nodes[node2].add_weight(node1, weight)
-        else:
-            second_node = Node(node2)
-            second_node.add_weight(node1, weight)
-            self.nodes[node2] = second_node
-
-    # Solve shortest-path using Dijkstra's Algorithm.
-    def solve_shortest_path(self, source):
+class Dijkstras(Graph):
+    def shortest_path(self, source):
         distances = {}
         previous_nodes = {}
         pq = PriorityQueue()
 
         # Init single-source and insert each vertex in the priority queue
         distances[source] = 0
-        for key in self.nodes:
-            if key != source:
-                distances[key] = float("inf")
-                previous_nodes[key] = "None"
-            pq.put((distances[key], key))
+        for vertex in self.vertices:
+            if vertex != source:
+                distances[vertex] = float("inf")
+                previous_nodes[vertex] = "None"
+            pq.put((distances[vertex], vertex))
 
         while not pq.empty():
             # Get vertex with min distance from PQ
             curr_set = pq.get()
-            curr_node = curr_set[1]
-            for key in self.nodes[curr_node].weights:
-                print("Current distances are: ", distances)
-                # Relax
-
+            curr_vertex = curr_set[1]
+            for adjacent_vertex in self.edges[curr_vertex]:
                 # Check if we can decrease the distance
-                possible_value = distances[curr_node] + \
-                    self.nodes[curr_node].weights[key]
+                possible_value = distances[curr_vertex] + \
+                    self.edges[curr_vertex][adjacent_vertex]
 
                 # If so, update our dictionaries and the PQ
-                if possible_value < distances[key]:
-                    distances[key] = possible_value
-                    previous_nodes[key] = curr_node
-                    # Decrease key
-                    pq.put((possible_value, key))
+                if possible_value < distances[adjacent_vertex]:
+                    distances[adjacent_vertex] = possible_value
+                    previous_nodes[adjacent_vertex] = curr_vertex
+                    # Decrease key in PQ.
+                    pq.put((possible_value, adjacent_vertex))
         return distances
 
 
 def main():
-    # Example graph
-    graph = Graph()
-    graph.add_edge("A", "B", 6)
-    graph.add_edge("A", "D", 1)
-    graph.add_edge("B", "D", 2)
-    graph.add_edge("D", "E", 1)
-    graph.add_edge("B", "E", 2)
-    graph.add_edge("B", "C", 5)
-    graph.add_edge("E", "C", 5)
-    print("Shortest-paths are: ", graph.solve_shortest_path("A"))
+    graph = Dijkstras(GraphType.DIRECTED)
+    graph.add_vertices(["A", "B", "C", "D", "E"])
+    graph.add_edges([("A", "B", 6), ("A", "D", 1), ("B", "D", 2),
+                    ("D", "E", 1), ("B", "E", 2), ("B", "C", 5), ("E", "C", 5)])
+    print("Shortest-paths are: ", graph.shortest_path("A"))
 
 
 if __name__ == "__main__":
