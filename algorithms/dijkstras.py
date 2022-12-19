ef21c52e2a72217bad640825c5ba4322f2319772
max-muoto <41130755+max-muoto@users.noreply.github.com>
2022-12-18 18:27:54 -0600

    Small fixes

--- algorithms/dijkstras.py
+++ algorithms/dijkstras.py
@@ -2,11 +2,11 @@ from queue import PriorityQueue
 
 from utility_classes.graph import Graph
 
+
 class Dijkstras:
     @staticmethod
     def shortest_path(graph: Graph, source: str):
-        vertices = graph.vertices
-        edges = graph.edges
+        vertex_adjs = graph.edges
 
         distances = {}
         previous_nodes = {}
@@ -14,7 +14,7 @@ class Dijkstras:
 
         # Init single-source and insert each vertex in the priority queue
         distances[source] = 0
-        for vertex in vertices:
+        for vertex in vertex_adjs:
             if vertex != source:
                 distances[vertex] = float("inf")
                 previous_nodes[vertex] = "None"
@@ -24,11 +24,11 @@ class Dijkstras:
             # Get vertex with min distance from PQ
             curr_set = pq.get()
             curr_vertex = curr_set[1]
-            for adjacent_vertex in edges[curr_vertex]:
+            for adjacent_vertex in vertex_adjs[curr_vertex]:
                 # Check if we can decrease the distance
                 possible_value = (
                     distances[curr_vertex] +
-                    edges[curr_vertex][adjacent_vertex]
+                    vertex_adjs[curr_vertex][adjacent_vertex]
                 )
 
                 # If so, update our dictionaries and the PQ
--- algorithms/prims.py
+++ algorithms/prims.py
@@ -5,14 +5,13 @@ from utility_classes.graph import Graph, GraphType
 class Prims:
     @staticmethod
     def minimum_spanning_tree(graph: Graph, root: str):
-        vertices = graph.vertices
-        edges = graph.edges
+        vertex_adjs = graph.edges
 
         parents = {}
         min_dist = {}
         pq = []
         min_dist[root] = 0
-        for vertex in vertices:
+        for vertex in vertex_adjs:
             if vertex != root:
                 min_dist[vertex] = float("inf")
             parents[vertex] = None
@@ -21,10 +20,10 @@ class Prims:
         while pq:
             curr = heapq.heappop(pq)
             curr_vertex = curr[1]
-            for vertex in edges[curr_vertex]:
-                if (min_dist[vertex], vertex) in pq and edges[curr_vertex][vertex] < min_dist[vertex]:
+            for vertex in vertex_adjs[curr_vertex]:
+                if (min_dist[vertex], vertex) in pq and vertex_adjs[curr_vertex][vertex] < min_dist[vertex]:
                     parents[vertex] = curr_vertex
-                    min_dist[vertex] = edges[curr_vertex][vertex]
+                    min_dist[vertex] = vertex_adjs[curr_vertex][vertex]
                     heapq.heappush(pq, (min_dist[vertex], vertex))
 
         return parents
