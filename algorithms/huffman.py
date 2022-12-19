904feecaccd9f0b3484e36886648b4f39cdd1412
max-muoto <41130755+max-muoto@users.noreply.github.com>
2022-12-18 19:42:22 -0600

    Improvement

--- algorithms/huffman.py
+++ algorithms/huffman.py
@@ -9,25 +9,27 @@ class HuffmanNode(TreeNode):
         self.freq = freq
 
 
-def huffman(c):
-    n = len(c)
-    heapq.heapify(c)
-    for _ in range(0, n-1):
-        x = heapq.heappop(c)
-        y = heapq.heappop(c)
-        freq = x[0] + y[0]
-        left = x[1]
-        right = y[1]
-        z = HuffmanNode(None, freq, left, right)
-        heapq.heappush(c, (freq, z))
-    return heapq.heappop(c)
+class Huffman:
+    @staticmethod
+    def compress(nodes):
+        n = len(nodes)
+        heapq.heapify(nodes)
+        for _ in range(0, n-1):
+            x = heapq.heappop(nodes)
+            y = heapq.heappop(nodes)
+            freq = x[0] + y[0]
+            left = x[1]
+            right = y[1]
+            z = HuffmanNode(None, freq, left, right)
+            heapq.heappush(nodes, (freq, z))
+        return heapq.heappop(nodes)
 
 
 def main():
     chars = [(5, HuffmanNode("A", 5)), (9, HuffmanNode("E", 9)), (12, HuffmanNode("C", 12)),
              (13, HuffmanNode("B", 13)), (16, HuffmanNode("D", 16)), (45, HuffmanNode("A", 45))]
     # Root node for the tree
-    node = huffman(chars)[1]
+    node = Huffman.compress(chars)[1]
     print(node.freq)
 
 
