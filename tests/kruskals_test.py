23a916621b001a0477b42fa04d06dcd588dddbaf
Maxwell Muoto <41130755+max-muoto@users.noreply.github.com>
2022-12-29 21:33:47 -0600

    Revert

--- tests/kruskals_test.py
+++ tests/kruskals_test.py
@@ -19,7 +19,8 @@ class KruskalsTest(unittest.TestCase):
         graph.add_edge("F", "C", 9)
         mst = {("A", "B"), ("F", "E"), ("C", "E"), ("C", "D"), ("A", "C")}
 
-        self.assertEqual(Kruskals.minimum_spanning_tree(graph), mst)
+        kruskals = Kruskals()
+        self.assertEqual(kruskals.minimum_spanning_tree(graph), mst)
 
 
 if __name__ == "__main__":
