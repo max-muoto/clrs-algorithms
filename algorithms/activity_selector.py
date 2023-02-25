76992890311e38fc01caaf15d7134b625b9f2627
max-muoto <41130755+max-muoto@users.noreply.github.com>
2023-02-25 15:16:23 -0600

    Fix CI

--- algorithms/activity_selector.py
+++ algorithms/activity_selector.py
@@ -14,7 +14,7 @@ def main():
     # Example from CLRS page 15.1
     s = [1, 3, 0, 5, 3, 5, 6, 7, 8, 2, 12]
     f = [4, 5, 6, 7, 9, 9, 10, 11, 12, 14, 16]
-    n = len(s) -    1
+    n = len(s) - 1
     # Our resulting set contains the index of each mutually compatiable activity.
     result = recursive_activity_selector(s, f, 0, n)
     print(result)
